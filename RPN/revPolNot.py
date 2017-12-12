f6e4c85618cb68ac108d0be18aad07b3d43050ff
Jeff <nigma_x@hotmail.com>
2017-12-11 20:46:12 -0600

    Added 4 new buttons Clearscreen, backspace, 0 and = Added 3 new commands equal = send the display string to the rpn_method and return an int clearscreen sets the display to "" backspace removes one char from the display string each click updated the rpn_method to pass in a string value and return an int

--- RPN/revPolNot.py
+++ RPN/revPolNot.py
@@ -1,13 +1,13 @@
 import operator
 from Tkinter import *
 
-def rpn_method():
+def rpn_method(rpn_string):
     # use a char operator symbal to do an operator method by passing the two value
     ops_list = { "+": operator.add,"-": operator.sub,"*": operator.mul,"/": operator.div}
 
     #  this holds the test value for running this method
-    rpn_string = "15 7 1 1 + - / 3 * 2 1 1 + + -"
-    rpn_list = rpn_string.split(" ")
+
+    rpn_list = rpn_string.split(".")
 
     # define that rpn_stack is a list and to empty the list
     rpn_stack = [0]
@@ -25,16 +25,35 @@ def rpn_method():
         else:
             rpn_stack.append(int(value))
 
-    print "Final answer: {0}".format(rpn_stack.pop())
+    return rpn_stack.pop()
 
 class RPN_GUI(Frame):
+
+    ops_char_list = ["+", "-","*","/"]
+
     def buttonValue(self,value):
-        self.RPNDisplay["text"] += value + " "
-        print value
+        if value in RPN_GUI.ops_char_list:
+            self.RPNDisplay["text"] += "." + value
+        else:
+            self.RPNDisplay["text"] += value
+
+    def clearScreen(self):
+        self.RPNDisplay["text"] = ""
+
+    def backSpace(self):
+        display_len = len(self.RPNDisplay["text"]) - 1
+        if(display_len < 0):
+            return
+        self.RPNDisplay["text"] = self.RPNDisplay["text"][:display_len]
+
+    def equalPress(self):
+        tempDisplay = self.RPNDisplay["text"]
+        if(tempDisplay == ""):
+            return
+        self.RPNDisplay["text"] = str(rpn_method(tempDisplay))
+
 
     def createWidgets(self):
-
-        self.ops_char_list = ["+","-","*","/"]
 
         self.RPNDisplay = Label(self)
         self.RPNDisplay["text"] = ""
@@ -44,13 +63,25 @@ class RPN_GUI(Frame):
         self.RPNDisplay["fg"] = "red"
         self.RPNDisplay.place(x=10,y=10)
 
-
-
         self.Exit = Button(self)
         self.Exit["text"] = "OFF"
         self.Exit["font"] = "Verdana 15 bold"
         self.Exit["command"] = self.quit
         self.Exit.place(x=235, y=350)
+
+        self.Exit = Button(self)
+        self.Exit["text"] = "<-"
+        self.Exit["font"] = "Verdana 15 bold"
+        self.Exit["command"] = self.backSpace
+        self.Exit.place(x=115, y=350)
+
+        self.Exit = Button(self)
+        self.Exit["text"] = "Clr"
+        self.Exit["font"] = "Verdana 15 bold"
+        self.Exit["command"] = self.clearScreen
+        self.Exit.place(x=10, y=350)
+
+
 
         for x in range(3):
             for y in range(3):
@@ -62,7 +93,7 @@ class RPN_GUI(Frame):
                 self.ValueButton.place(x=x * 60 + 30, y=y * -60 + 220)
         y = 0
         x = 3
-        for opsChar in self.ops_char_list:
+        for opsChar in RPN_GUI.ops_char_list:
             self.ValueButton = Button(self)
             buttonVal = opsChar
             self.ValueButton["text"] = buttonVal
@@ -83,6 +114,27 @@ class RPN_GUI(Frame):
         self.ValueButton["text"] = buttonVal
         self.ValueButton["font"] = "Verdana 15 bold"
         self.ValueButton["command"] = lambda n=buttonVal: self.buttonValue(n)
+        self.ValueButton.place(x=2 * 60 + 30, y=0 * -60 + 280)
+
+        self.ValueButton = Button(self,height=2, width=2)
+        buttonVal = "."
+        self.ValueButton["text"] = "Sp"
+        self.ValueButton["font"] = "Verdana 10 bold"
+        self.ValueButton["command"] = lambda n=buttonVal: self.buttonValue(n)
+        self.ValueButton.place(x=0 * 60 + 30, y=0 * -60 + 280)
+
+        self.ValueButton = Button(self)
+        buttonVal = 0
+        self.ValueButton["text"] = buttonVal
+        self.ValueButton["font"] = "Verdana 15 bold"
+        self.ValueButton["command"] = lambda n=str(buttonVal): self.buttonValue(n)
+        self.ValueButton.place(x=1 * 60 + 30, y=0 * -60 + 280)
+
+        self.ValueButton = Button(self)
+        buttonVal = "="
+        self.ValueButton["text"] = buttonVal
+        self.ValueButton["font"] = "Verdana 15 bold"
+        self.ValueButton["command"] = self.equalPress
         self.ValueButton.place(x=2 * 60 + 30, y=0 * -60 + 280)
 
     def __init__(self, master=None):
